813fa7731954ed24b8716b327caa710c8880f8a1
Ben Terry <benterry@Bens-MacBook.local>
2019-05-16 16:52:38 -0400

    Completed up to video 18

--- src/index.js
+++ src/index.js
@@ -3,34 +3,42 @@ import React from 'react';
 
 console.log('index.js is running...');
 
-const userName = 'Ben';
-const age = 25;
-const location = 'Shillington'
-
-const appObj = {
+const root = document.getElementById('root');
+const app = {
     title: 'Indecision App',
-    subtitle: 'Having a problem deciding what to do?'
+    subtitle: 'Having a hard time deciding what to do?',
+    options: []
 };
 
-let template = (
-    <div>
-        <h1>{appObj.title}</h1>
-        <p>{appObj.subtitle}</p>
-        <ol>
-            <li>This is item 1</li>
-            <li>This is item 2</li>
-        </ol>
-    </div>
-);
+const onFormSubmit = (e) => {
+    e.preventDefault();
+    const option = e.target.elements.option.value;
+    if (option){
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        renderApp();
+    }
+};
 
-let templateTwo = (
-    <div>
-        <h1>{userName}</h1>
-        <p>Age: {age}</p>
-        <p>Location: {location}</p>
-    </div>
-);
+const renderApp = () => {
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options.length !== 0 ? 'Here are your options' : 'No options'}</p>
+            <p>The options length is: {app.options.length}</p>
+            <ol>
+                <li>Item one</li>
+                <li>Item two</li>
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option"></input>
+                <button>Add Option</button>
+            </form>
+        </div>
+    );
+    
+    ReactDOM.render(template, root);
+};
 
-let root = document.getElementById('root');
-
-ReactDOM.render(template, root);
+renderApp();
