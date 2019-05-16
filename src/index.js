5a706bba2ad67aa2f99125877498fdcf18116a87
Ben Terry <beterry12@gmail.com>
2019-05-15 21:10:15 -0400

    completed up to video 11

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+console.log('index.js is running...');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const userName = 'Ben';
+const age = 25;
+const location = 'Shillington'
+
+const appObj = {
+    title: 'Indecision App',
+    subtitle: 'Having a problem deciding what to do?'
+};
+
+let template = (
+    <div>
+        <h1>{appObj.title}</h1>
+        <p>{appObj.subtitle}</p>
+        <ol>
+            <li>This is item 1</li>
+            <li>This is item 2</li>
+        </ol>
+    </div>
+);
+
+let templateTwo = (
+    <div>
+        <h1>{userName}</h1>
+        <p>Age: {age}</p>
+        <p>Location: {location}</p>
+    </div>
+);
+
+let root = document.getElementById('root');
+
+ReactDOM.render(template, root);
