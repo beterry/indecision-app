58e5645bd01714aa1da20ca4e850b7d27875da3d
Ben Terry <beterry12@gmail.com>
2019-05-23 20:37:16 -0400

    Completed up to video 47

--- src/index.js
+++ src/index.js
@@ -10,6 +10,25 @@ class App extends React.Component {
         this.handleDeleteOption = this.handleDeleteOption.bind(this);
         this.state = {options: this.props.options};
     }
+    componentDidMount(){
+        console.log('fetching data...');
+        try{
+            const json = localStorage.getItem('options');
+            const options = JSON.parse(json);
+            if(options){
+                this.setState({options});
+            }
+        }catch(e){
+            console.log(e);
+        }
+    }
+    componentDidUpdate(prevProps, prevState){
+        if(prevState.options.length !== this.state.options.length){
+            const json = JSON.stringify(this.state.options);
+            localStorage.setItem('options', json);
+            console.log('saving data...');
+        }
+    }
     handleDeleteOptions(){
         this.setState({options: []});
     }
@@ -82,6 +101,7 @@ const Options = (props) => {
     return (
         <div>
             <button onClick={props.handleDeleteOptions}>Remove All</button>
+            {props.options.length === 0 && <p>Add an item to get started</p>}
             <ol>
                 {props.options.map((option) => (
                     <Option
@@ -115,6 +135,9 @@ class AddOption extends React.Component {
         const option = e.target.elements.option.value.trim();
         const error = this.props.handleAddOption(option);
         this.setState({error});
+        if(!error){
+            e.target.elements.option.value = "";
+        }
     }
     render() {
         return (
